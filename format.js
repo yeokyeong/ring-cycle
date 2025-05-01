a4ea922f670557636aa872c926ff64c1b25522f5
= <yeokyeong1004@gmail.com>
2025-05-01 22:41:56 +0900

    add : testData & formatting

--- format.js
+++ format.js
@@ -7,11 +7,28 @@ function convertTo24Hour(hour, midday) {
   }
 }
 
-function formatDate(date) {
+export function formatDate(date) {
   const hour = date.getHours();
   const minute = date.getMinutes();
   return [
     hour < 10 ? "0" + hour : hour,
     minute < 10 ? "0" + minute : minute,
   ].join(":");
+}
+
+// 현재 시:분을 "HH:MM" 형태로 리턴하는 함수
+function getNowTimeString() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  return `${hours}:${minutes}`;
+}
+
+export function getDateOnly(timestamp) {
+  const date = new Date(timestamp);
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate()
+  ).getTime();
 }
